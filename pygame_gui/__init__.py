f54d09d39b4a7055858c6525f71fbfe9267648c4
Devondint <duonganhkhoi19082008@gmail.com>
2025-04-26 22:05:41 +0700

    Tạo file storage và thêm __init__ cho thư viện

--- pygame_gui/__init__.py
+++ pygame_gui/__init__.py
@@ -1,3 +1,20 @@
-import pygame
+import pygame, threading
 
 import document, style, script
+import storage
+
+def __init__(title: str, logo: pygame.Surface, mode_display: str = 'resizable', size: tuple[int, int] = (0,0)):
+	pygame.init()
+	pygame.display.set_caption(title)
+	pygame.display.set_icon(logo)
+	
+	mode_display = mode_display.lower()
+	if mode_display == 'resizable':
+		pygame.display.set_mode(size, pygame.RESIZABLE)
+	elif mode_display == 'fullscreen':
+		pygame.display.set_mode(size, pygame.FULLSCREEN)
+	elif mode_display == 'noframe':
+		pygame.display.set_mode(size, pygame.NOFRAME)
+	else:
+		pygame.display.set_mode(size, pygame.SHOWN)
+	return None
