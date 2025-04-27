3c24846079b04518d03193ee495f5ccd1348edc2
Devondint <duonganhkhoi19082008@gmail.com>
2025-04-27 10:19:33 +0700

    Thiết lập cấu trúc cho document.py

---
+++ pygame_gui/document.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Group:
+    def __init__(self, name, children = None):
+        pass
+    
+class Text:
+	def __init__(self, name, text):
+		pass
+
+class Line:
+	def __init__(self, name, points):
+
+		pass
+
+class Shape:
+	def __init__(self, name, points):
+		pass
+
+class ShapeRectangle:
+	def __init__(self, name, rect):
+		pass
+
+class ShapeEllipse:
+	def __init__(self, name, rect):
+		pass
+class Image:
+	def __init__(self, name, image_path):
+		pass
+
+class Input:
+	def __init__(self, name, input_type):
+		pass
+
+class Link:
+	def __init__(self, name, link_address):
+		pass
